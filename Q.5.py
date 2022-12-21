8e7ee0771b97648f0cb711bcf144ebaa73e2b937
Ansh Verma <118916802+Proestofpros@users.noreply.github.com>
2022-12-21 22:44:50 +0530

    Delete Q.5.py

--- Q.5.py
+++
@@ -1,9 +0,0 @@
-n = 6
-for i in range(n):
-    # printing spaces
-    for j in range(i):
-        print(' ', end='')
-    # printing alphabet
-    for j in range(2*(n-i)-1):
-        print(chr(65 + j), end='')
-    print()
